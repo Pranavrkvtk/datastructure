00c10a6febd5d266186706ee8a00573b98246343
Pranavrkvtk <94657009+Pranavrkvtk@users.noreply.github.com>
2022-03-11 07:30:14 +0530

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,94 +1,81 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-#define n 5
-int queue[n];
-int rear=-1;
-int front=-1;
-void insertion();
-void deletion();
+#define MAX 50
+void insert();
+void delete();
 void display();
+int queue_array[MAX];
+int rear = - 1;
+int front = - 1;
 int main()
 {
-	int ch;
-	while(1)
-	{
-		printf("\nSelect your choice \n 1.Insertion \n 2.deletion\n 3.display \n 4.exit \n");
-	scanf("%d",&ch);
-	switch(ch)
-	{
-		case 1:insertion();
-			break;
-		case 2:deletion();
-			break;
-		case 3:display();
-			break;
-		case 4:exit(0);
-		default :printf("select correct option");
-	}
-	}
-	return 0;
-}
-void insertion()
+int choice;
+while (1)
 {
-	int ele;
-	if (front==n-1)
-	{
-		printf("queue is full");
-	}
-	else if(rear==-1 && front==-1)
-	{
-		front=rear=0;
-		printf("enter the element=");
-		scanf("%d",&ele);
-		queue[rear]=ele;
-	}
-	else
-	{
-		printf("enter the element=");
-		scanf("%d",&ele);
-		rear++;
-		queue[rear]=ele;
-	}
-}
-void deletion()
+printf("1.Insert element to queue n");
+printf("2.Delete element from queue n");
+printf("3.Display all elements of queue n");
+printf("4.Quit n");
+printf("Enter your choice : ");
+scanf("%d", &choice);
+switch(choice)
 {
-	if(rear==-1)
-	{
-		printf("queue is empty");
-	}
-	else if(front==rear)
-	{
-		printf("deleted element is=%d",queue[front]);
-		front=rear=-1;
-		
-	}
-	else
-	{
-		front++;
-		printf("deleted element is = %d",queue[front]);
-		if (rear==front)
-		{
-			rear=front=-1;
-		}
-		
-	}
-	
+case 1:
+insert();
+break;
+case 2:
+delete();
+break;
+case 3:
+display();
+break;
+case 4:
+exit(1);
+default:
+printf("Wrong choice n");
+}
+}
+}
+void insert()
+{
+int item;
+if(rear == MAX - 1)
+printf("Queue Overflow n");
+else
+{
+if(front== - 1)
+front = 0;
+printf("Inset the element in queue : ");
+scanf("%d", &item);
+rear = rear + 1;
+queue_array[rear] = item;
+}
+}
+void delete()
+{
+if(front == - 1 || front > rear)
+{
+printf("Queue Underflow n");
+return;
+}
+else
+{
+printf("Element deleted from queue is : %dn", queue_array[front]);
+front = front + 1;
+}
 }
 void display()
 {
-	int i;
-	if (front==-1)
-	{
-		printf("queue is empty");
-	}
-	else
-	{
-		printf("list\n");
-		for(i=front+1;i<=rear;i++)
-		{
-			printf("%d\n",queue[i]);
-		}
-	}
+int i;
+if(front == - 1)
+printf("Queue is empty n");
+else
+{
+printf("Queue is : n");
+for(i = front; i <= rear; i++)
+printf("%d ", queue_array[i]);
+printf("n");
+}
 }
 	
 		
